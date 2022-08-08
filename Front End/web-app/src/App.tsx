1cf5f27cfcec4a926db3837f54cac0076f2d1c70
Kelvin Wong <kakoo40@gmail.com>
2022-08-09 11:55:06 +1200

    Stylising page

--- Front End/web-app/src/App.tsx
+++ Front End/web-app/src/App.tsx
@@ -5,16 +5,19 @@ import TextField from "@mui/material/TextField";
 import "./App.css";
 import { Box, Button, Grid, Paper, Skeleton } from "@mui/material";
 
+import SpeakerIcon from "./images/Speaker_Icon.png";
+
 function App() {
   const [word, setWord] = useState("");
   const [wordData, setWordData] = useState("");
 
   const DICTIONARY_BASE_API_URL =
     "https://api.dictionaryapi.dev/api/v2/entries/en/";
+
   return (
-    <div>
+    <div className="App">
       <div className="search-field">
-        <h1>Dictionary Search</h1>
+        <h1>Dictionary</h1>
         <div style={{ display: "flex", justifyContent: "center" }}>
           <TextField
             id="word-search-bar"
@@ -23,7 +26,7 @@ function App() {
             onChange={(prop) => {
               setWord(prop.target.value);
             }}
-            label="Enter a word to get a definition"
+            label="Enter a word"
             variant="outlined"
             placeholder="Search..."
             size="medium"
@@ -54,14 +57,16 @@ function App() {
             <Grid
               container
               direction="row"
-              spacing={5}
+              padding={2}
               sx={{
                 justifyContent: "center",
               }}
             >
               <Grid item>
                 <Box>
-                  {wordData === undefined || wordData === null ? (
+                  {wordData === undefined ||
+                  wordData === null ||
+                  wordData === "" ? (
                     <h1>Word not found</h1>
                   ) : (
                     <div>
@@ -70,27 +75,38 @@ function App() {
                           <h2>
                             {(wordData as any).word}{" "}
                             <button
+                              style={{
+                                width: "30px",
+                                height: "30px",
+                                backgroundImage: `url(${SpeakerIcon})`,
+                              }}
                               onClick={() => {
                                 playAudio();
                               }}
                             ></button>
                           </h2>
-                          <h4>Parts of speech:</h4>
-                          <p>{(wordData as any).meanings[0].partOfSpeech}</p>
-                          <h4>Definition:</h4>
-                          <p>
-                            {
-                              (wordData as any).meanings[0].definitions[0]
-                                .definition
-                            }
-                          </p>
-                          <h4>Example:</h4>
-                          <p>
-                            {
-                              (wordData as any).meanings[0].definitions[0]
-                                .example
-                            }
-                          </p>
+                          <Box sx={{ m: 2, p: 1, boxShadow: 3 }}>
+                            <h4>Parts of speech:</h4>
+                            <p>{(wordData as any).meanings[0].partOfSpeech}</p>
+                          </Box>
+                          <Box sx={{ m: 2, p: 1, boxShadow: 3 }}>
+                            <h4>Definition:</h4>
+                            <p>
+                              {
+                                (wordData as any).meanings[0].definitions[0]
+                                  .definition
+                              }
+                            </p>
+                          </Box>
+                          <Box sx={{ m: 2, p: 1, boxShadow: 3 }}>
+                            <h4>Example:</h4>
+                            <p>
+                              {
+                                (wordData as any).meanings[0].definitions[0]
+                                  .example
+                              }
+                            </p>
+                          </Box>
                         </div>
                       )}
                     </div>
@@ -106,13 +122,15 @@ function App() {
 
   function search() {
     console.log(word);
-    if (word === undefined || word === "") {
-      return;
-    }
 
-    axios.get(DICTIONARY_BASE_API_URL + word?.toLowerCase()).then((res) => {
-      setWordData(res.data[0]);
-    });
+    axios
+      .get(DICTIONARY_BASE_API_URL + word?.toLowerCase())
+      .then((res) => {
+        setWordData(res.data[0]);
+      })
+      .catch(() => {
+        setWordData("");
+      });
   }
 
   function playAudio() {
